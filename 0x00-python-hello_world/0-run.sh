728e882516e0ef22a7b5d6ebb8af4ecda3d7b661
kenyanroot <ndedaanaco@gmail.com>
2023-10-30 13:22:25 -0700

    fix

--- 0x00-python-hello_world/0-run.sh
+++ 0x00-python-hello_world/0-run.sh
@@ -1,16 +1,2 @@
 #!/bin/bash
-
-# Check if the PYFILE environment variable is set
-if [[ -z "$PYFILE" ]]; then
-  echo "The PYFILE environment variable is not set."
-  exit 1
-fi
-
-# Check if the specified file exists
-if [[ ! -f "$PYFILE" ]]; then
-  echo "The file $PYFILE does not exist."
-  exit 1
-fi
-
-# Run the Python script
 python3 "$PYFILE"
