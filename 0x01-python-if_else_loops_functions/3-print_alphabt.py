46e5dcefc3d68e74244021e7b1fe68defca5444f
kenyanroot <ndedaanaco@gmail.com>
2023-11-01 06:41:17 -0700

    3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 def print_alphabt():
-    print("{}".format(''.join([chr(i) for i in range(97, 123) if i != 101 and i != 113])), end='')
+    print("{}".format(''.join([chr(i) for i in range(97, 123) 
+                               if i != 101 and i != 113])), end='')
 
 
 if __name__ == "__main__":
