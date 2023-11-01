b6779c44bbb88c616fa6d709ce0bcf0fc1719c7e
kenyanroot <ndedaanaco@gmail.com>
2023-11-01 06:27:34 -0700

    yeey

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def print_alphabet():
-    for i in range(97, 123):  # ASCII values for 'a' to 'z' are 97 to 122 inclusive
-        print(chr(i), end="")
+    for i in range(97, 123):
+        print(chr(i), end='')
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print_alphabet()
