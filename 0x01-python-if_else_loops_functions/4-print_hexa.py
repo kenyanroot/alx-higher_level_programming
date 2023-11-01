3947625b83e0af66d9197cdc84cc6c7fac672716
kenyanroot <ndedaanaco@gmail.com>
2023-11-01 06:43:57 -0700

    4

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -4,5 +4,6 @@ def print_decimal_hexadecimal():
     for i in range(99):  # Iterate from 0 to 98
         print("{} = {}".format(i, hex(i)))
 
+
 if __name__ == '__main__':
     print_decimal_hexadecimal()
