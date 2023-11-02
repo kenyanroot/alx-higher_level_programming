30d58d31b4d60ec227eb38541ab9dd6bed716593
kenyanroot <ndedaanaco@gmail.com>
2023-11-02 14:42:58 -0700

    k

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,15 +1,4 @@
 #!/usr/bin/python3
-
-
-import sys
-
-
-def main():
-    total_sum = 0  # Initialize the sum at 0
-    for arg in sys.argv[1:]:
-        total_sum += int(arg)
-        print(total_sum)
-
-
 if __name__ == "__main__":
-    main()  # Call main if the script is run directly
+    from sys import argv
+    print(sum(int(arg) for arg in argv[1:]))
