ee2e2c29901a8333818a37779f1fb9a332c2742b
kenyanroot <ndedaanaco@gmail.com>
2023-11-20 13:30:20 -0800

    mod

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -19,16 +19,3 @@ def safe_print_list(my_list=[], x=0):
             break
     print()  # New line after printing list elements
     return count
-
-
-# Test cases
-my_list = [1, 2, 3, 4, 5]
-
-nb_print = safe_print_list(my_list, 2)
-print("nb_print: {:d}".format(nb_print))
-
-nb_print = safe_print_list(my_list, 5)
-print("nb_print: {:d}".format(nb_print))
-
-nb_print = safe_print_list(my_list, 7)
-print("nb_print: {:d}".format(nb_print))
