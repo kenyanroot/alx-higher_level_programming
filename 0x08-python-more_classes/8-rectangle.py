bf2e40d0741a45686c152dccbea7ecca6b2c8c06
kenyanroot <46379959+kenyanroot@users.noreply.github.com>
2023-11-28 04:19:14 +0300

    pycodestyle

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 """Defines a Rectangle class."""
 
 
@@ -104,3 +105,4 @@ class Rectangle:
         """Print a message for every deletion of a Rectangle."""
         type(self).number_of_instances -= 1
         print("Bye rectangle...")
+        
