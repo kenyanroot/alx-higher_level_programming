25325211ee691fb87b9c7182f35a33fe2a7bf055
anaco <ndedaanaco@gmail.com>
2024-04-12 18:14:33 +0300

    small update

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # send POST HTTP request with values for `email` and `subject` variables
-curl -sd 'email=hr@holbertonschool.com&subject=I will always be here for PLD' "$1"
+curl -sd 'email=test@gmail.com&subject=I will always be here for PLD' "$1"
