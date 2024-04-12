0b845212bb6a61aa457796005faf868a637c3876
anaco <ndedaanaco@gmail.com>
2024-04-12 19:08:12 +0300

    wueeh 3

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-# Sends a GET request to the provided URL and displays the body of the response if the status code is 200
-
-url=$1
-response=$(curl -s -o /dev/null -w "%{http_code}" "$url")
-if [ "$response" -eq 200 ]; then curl -s "$url"; fi
+# Sends a GET request to a URL and displays the body of the response only for a 200 status code
+curl -s -L "$1" -o /dev/null -w "%{http_code}" | grep -q "200" && curl -s -L "$1"
