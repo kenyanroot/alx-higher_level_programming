6dd9dee3706152deda50c8a7ef4abf8dfa627192
anaco <ndedaanaco@gmail.com>
2024-04-12 19:39:38 +0300

    more minor corrections 2

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
-// searches the second biggest integer in the list of arguments.
+// Searches the second biggest integer in the list of arguments.
 
 if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  const list = process.argv.sort();
-  console.log(list.reverse()[1]);
+  let nums = process.argv.slice(2).map(Number); // Convert arguments to numbers
+  nums = nums.sort((a, b) => a - b); // Sort numbers in ascending order
+  console.log(nums[nums.length - 2]); // Output the second largest number
 }
