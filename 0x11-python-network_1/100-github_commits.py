33d341e933dbf08445562b6554af44489f88bcba
anaco <ndedaanaco@gmail.com>
2024-04-12 18:44:26 +0300

    corrections 4

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,23 +1,18 @@
 #!/usr/bin/python3
-"""0x11. Python - Network #1, task 10. Time for an interview!
-"""
+"""A script that fetches the 10 most recent commits from a given repository and user."""
+
+import requests
+import sys
 
 if __name__ == "__main__":
-    from requests import get
-    from sys import argv
+    repo_name = sys.argv[1]
+    owner_name = sys.argv[2]
+    url = f"https://api.github.com/repos/{owner_name}/{repo_name}/commits"
 
-    def commit_date(elem):
-        """ Retruns date of commit for sorting of `commit_list`
-        """
-        return elem.get('commit').get('author').get('date')
+    response = requests.get(url)
+    commits = response.json()
 
-    url = 'https://api.github.com/repos/{}/{}/commits'.format(argv[2], argv[1])
-    response = get(url)
-    commit_list = response.json()
-    list_by_date = sorted(commit_list, key=commit_date, reverse=True)
-    for i, obj_dict in enumerate(list_by_date):
-        if i > 9:
-            break
-        print('{}: {}'.format(obj_dict.get('sha'),
-                              obj_dict.get(
-                                  'commit').get('author').get('name')))
+    for commit in commits[:10]:
+        sha = commit.get('sha')
+        author_name = commit.get('commit').get('author').get('name')
+        print(f"{sha}: {author_name}")
