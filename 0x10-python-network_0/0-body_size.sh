3d8aa70803ba1ef46cf0bde08a40effd3f7af9f3
anaco <ndedaanaco@gmail.com>
2024-04-12 19:06:09 +0300

    wueeh 2

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a request to a URL and displays the size of the response body in bytes
-curl -s "$1" -o /dev/null -w '%{size_download}\n'
+curl -s "$1" -w "%{size_download}\n" -o /dev/null
--- 0x10-python-network_0/start_web_server.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-kill -9 `cat web.pid` > /dev/null 2>&1;
-sleep 2;
-
-python3 "$1" > /dev/null 2>&1 &
-echo $! > web.pid
-
-sleep 5;
