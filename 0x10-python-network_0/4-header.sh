6b7148405e40f973712cf7099e2965b09c53d3e1
anaco <ndedaanaco@gmail.com>
2024-04-12 18:18:06 +0300

    small update

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# send GET HTTP request with header variable `X-HolbertonSchool-User-Id` of 98
-curl -sH 'X-HolbertonSchool-User-Id:98' "$1"
+# send GET HTTP request with header variable `X-School-User-Id` of 98
+curl -sH 'X-School-User-Id:98' "$1"
