76942bd5569f94e833a37a995729d34e695324a2
anaco <ndedaanaco@gmail.com>
2024-04-12 23:53:58 +0300

    hhh

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,9 +1,15 @@
 #!/usr/bin/node
-// function that returns the number of occurrences in a list
+// Script that inverts a dictionary to group user IDs by their occurrence counts.
 
-const originalList = require('./100-data').list;
-console.log(originalList);
-const mappedList = originalList.map (function (e, index) {
-  return (e * index);
+const dict = require('./101-data').dict;
+const newDict = {};
+
+Object.keys(dict).forEach(key => {
+  const value = dict[key];
+  if (!newDict[value]) {
+    newDict[value] = [];
+  }
+  newDict[value].push(key);
 });
-console.log(mappedList);
+
+console.log(newDict);
