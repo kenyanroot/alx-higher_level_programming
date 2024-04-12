3f111a4422cf11331ef154ac7bcd93ffb0b3ceaa
anaco <ndedaanaco@gmail.com>
2024-04-12 19:01:51 +0300

    wueeh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-# Sends a request to the provided URL and displays the size of the response body in bytes
-
-url=$1
-size=$(curl -s -o /dev/null -w "%{size_download}" "$url")
-echo "$size"
+# Sends a request to a URL and displays the size of the response body in bytes
+curl -s "$1" -o /dev/null -w '%{size_download}\n'
